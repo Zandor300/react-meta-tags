249c65a1936fb41b3597567330a8d78c9d001b61
Zandor Smith <info@zsinfo.nl>
2022-10-26 12:38:35 +0200

    fix: Fix TypeScript types.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 export interface ExtractFunction {
   (elements: React.ReactElement | React.ReactElement[]): void;
@@ -6,10 +6,10 @@ export interface ExtractFunction {
 
 export class MetaTagsContext extends React.Component<{
   extract: ExtractFunction;
-}> {};
-  
-export class MetaTags extends React.Component {};
-  
-export class ReactTitle extends React.Component<{ title: string }> {};
+}> {}
 
-export default MetaTags;
+export class MetaTags extends React.Component {}
+
+export class ReactTitle extends React.Component<{ title: string }> {}
+
+export default MetaTags
